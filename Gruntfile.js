10e2de096717e4fe031306df4316e0070972eace
Alberto Varela <alberto@berriart.com>
2015-11-27 00:22:45 +0100

    Adding world json file to dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -229,7 +229,8 @@ module.exports = function (grunt) {
                         '*.{ico,png,txt}',
                         '.htaccess',
                         'images/{,*/}*.{webp,gif}',
-                        'styles/{,*/}*.*'
+                        'styles/{,*/}*.*',
+                        'data/*.*'
                     ]
                 }]
             },
